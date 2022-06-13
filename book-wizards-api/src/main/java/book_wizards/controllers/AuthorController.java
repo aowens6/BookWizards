636379e79caa4fb118a7f4c869a9304895a01054
aowens6 <austyn.owens1@gmail.com>
2022-06-13 13:50:56 -0500

    controllers

---
+++ book-wizards-api/src/main/java/book_wizards/controllers/AuthorController.java
@@ -0,0 +1,64 @@
+package book_wizards.controllers;
+
+import book_wizards.domain.AuthorService;
+import book_wizards.domain.Result;
+import book_wizards.models.Author;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = {"http://localhost:3000"})
+@RequestMapping("/api/author")
+public class AuthorController {
+
+  private final AuthorService service;
+
+
+  public AuthorController(AuthorService service) {
+    this.service = service;
+  }
+
+  @GetMapping
+  public List<Author> findAll() {
+    return service.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public Author findById(@PathVariable int id) {
+    return service.findById(id);
+  }
+
+  @PostMapping
+  public ResponseEntity<Object> add(@RequestBody Author author) {
+    Result<Author> result = service.add(author);
+    if (result.isSuccess()) {
+      return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
+    }
+    return ErrorResponse.build(result);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Object> update(@PathVariable int id, @RequestBody Author author) {
+    if (id != author.getAuthorId()) {
+      return new ResponseEntity<>(HttpStatus.CONFLICT);
+    }
+
+    Result<Author> result = service.update(author);
+    if (result.isSuccess()) {
+      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    return ErrorResponse.build(result);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteById(@PathVariable int id) {
+    if (service.deleteById(id)) {
+      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+  }
+}
