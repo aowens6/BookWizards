decd8e14310a0aa4f4e916b0ec34d84035ba60ac
bbnyamee <ebrenda75@gmail.com>
2022-06-16 14:25:31 -0400

    new changes

--- book-wizards-api/src/test/java/book_wizards/domain/MeetingServiceTest.java
+++ book-wizards-api/src/test/java/book_wizards/domain/MeetingServiceTest.java
@@ -6,14 +6,18 @@ import book_wizards.models.Book;
 import book_wizards.models.Meeting;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class MeetingServiceTest {
 
     @Autowired
@@ -31,13 +35,22 @@ class MeetingServiceTest {
 
     @Test
     void findById() {
+        Meeting meeting = new Meeting();
+        //when(repository.existsById(anyInt())).thenReturn(meeting);
+        assertEquals(meeting, service.findById(anyInt()));
     }
 
     @Test
     void add() {
+        Meeting meeting = new Meeting();
+        meeting.setDescription("test");
+        meeting.setGroupName("bookWizards");
+//        meeting.setStartDateTime();
+//        meeting.setEndDateTime();
 
+        Result<Meeting> actual =  service.add(meeting);
 
-
+        assertEquals(ResultType.SUCCESS, actual.getType());
 
     }
 
@@ -49,5 +62,11 @@ class MeetingServiceTest {
     void deleteById() {
         when(repository.existsById(3)).thenReturn(true);
         assertTrue(service.deleteById(3));
+    }
+
+    @Test
+    void shouldNotDeleteById(){
+        when(repository.existsById(3)).thenReturn(false);
+        assertFalse(service.deleteById(3));
     }
 }
